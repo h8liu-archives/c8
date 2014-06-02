cb76cb2859af45c2c5b09cc1a04172732e04d815
He Liu <liulonnie@gmail.com>
2014-06-01 18:17:29 -0700

    file

--- c8go/fs/file.go
+++ c8go/fs/file.go
@@ -1,8 +1,19 @@
 package fs
 
+import (
+	"bytes"
+)
+
 type File struct {
 	perm    uint32
-	content []byte
+	content *byte.Buffer
+}
+
+func NewFile(perm uint32) *File {
+	ret := new(File)
+	ret.perm = perm
+	ret.content = new(byte.Buffer)
+	return ret
 }
 
 func (f *File) Perm() uint32 { return f.perm }
