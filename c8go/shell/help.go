757d2da7460b597c30c0d84d63b479f04c030159
He Liu <liulonnie@gmail.com>
2014-06-04 12:31:37 -0700

    help

--- c8go/shell/help.go
+++ c8go/shell/help.go
@@ -8,15 +8,15 @@ import (
 
 var helpStr = strings.TrimSpace(`
 supported built-in commands:
-	ls     list the content of the current working directory
-	pwd    print the current working directory
-	mkdir  make a new directory
-	rm     remove a file or an empty directory
-	cp     copy a file
-	mv     move a file
-	cat    print the content of a file
-	echo   print the args, separated by spaces
-	help   print this help message
+    ls     list the content of the current working directory
+    pwd    print the current working directory
+    mkdir  make a new directory
+    rm     remove a file or an empty directory
+    cp     copy a file
+    mv     move a file
+    cat    print the content of a file
+    echo   print the args, separated by spaces
+    help   print this help message
 `)
 
 func help(args []string, out io.Writer) int {
