26c043fd10fb388cd277809a9c2cdc7f540aa464
Ryan Huang <monkeyeof@gmail.com>
2014-06-05 17:20:04 -0700

    test commit: add args support for ls

--- c8go/shell/ls.go
+++ c8go/shell/ls.go
@@ -8,28 +8,50 @@ import (
 )
 
 func ls(args []string, out io.Writer) int {
-	if len(args) != 1 {
-		fmt.Fprintf(out, "ls with args not implemented yet\n")
-		return -1
-	}
-
-	node := fileSys.Get(Pwd)
-	dir, okay := node.(*fs.Dir)
-	if !okay {
-		fmt.Fprintf(out, "error: current working directory does not exist\n")
-		return -1
-	}
-
-	lst := dir.List()
-	for i, p := range lst {
-		if i > 0 {
-			fmt.Fprint(out, " ")
-		}
-		fmt.Fprint(out, p)
-	}
-	if len(lst) > 0 {
-		fmt.Fprintln(out)
-	}
+  var items []string = []string{}
+  var long bool
+  for _, arg := range(args[1:]) {
+    if arg == "-l" {
+      long = true
+    } else {
+      items = append(items, arg)
+    }
+  }
+  if len(items) == 0 {
+    items = []string{Pwd}
+  }
+  for it, item:= range(items) {
+    if len(items) > 1 {
+		  fmt.Fprintln(out, item + ":")
+    }
+    if item[0] != '/' {
+      item = Pwd + item
+    }
+    node := fileSys.Get(item)
+    dir, okay := node.(*fs.Dir)
+    if !okay {
+      fmt.Fprintf(out, "error: current working directory does not exist\n")
+      return -1
+    }
+    lst := dir.List()
+    for i, p := range lst {
+      if !long && i > 0 {
+        fmt.Fprint(out, " ")
+      }
+      fmt.Fprint(out, p)
+      if long {
+        fmt.Fprintln(out)
+      }
+    }
+    if len(lst) > 0 {
+      if !long {
+        fmt.Fprintln(out)
+      }
+      if len(items) > 1  && it != len(items) -1 {
+        fmt.Fprintln(out)
+      }
+    }
+  }
 
 	return 0
 }
